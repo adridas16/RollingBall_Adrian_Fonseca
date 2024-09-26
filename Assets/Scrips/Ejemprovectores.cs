4ca6d615f165a6ca3959f5c58c469d77dc7f2cf6
adridas16 <adridas666@gmail.com>
2024-09-26 20:58:44 +0200

    mapa

---
+++ Assets/Scrips/Ejemprovectores.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejemprovectores : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(3, 5, 0);
+        transform.eulerAngles = new Vector3(60,52,180);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
